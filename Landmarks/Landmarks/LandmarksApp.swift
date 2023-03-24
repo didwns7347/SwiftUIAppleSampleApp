0aa904298d4ef618e4068347afdec202af4b1821
양준수 <yangjs@markany.com>
2023-03-24 14:19:35 +0900

    프로젝트 생성

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by yangjs on 2023/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
